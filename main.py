690f9c1af4f105ba0c6825beebc49d0e820fef17
jlbrown5-ncat <60116362+jlbrown5-ncat@users.noreply.github.com>
2021-12-12 10:30:23 -0500

    Update

--- main.py
+++ main.py
@@ -8,13 +8,15 @@ import random
 if __name__ == '__main__':
     # This is the Publisher
     wait_interval = 60
+    BROKER_URL = "broker.mqttdashboard.com"
+    BROKER_PORT = 1883
 
     while True:
         status = random.randint(0, 1)
         amount = random.random() * 1000
 
         client = mqtt.Client()
-        client.connect("192.168.1.59", 1883, 60)
+        client.connect(BROKER_URL, BROKER_PORT)
         # client.publish("topic/test", "Hello world!")
         client.publish("light", status)
         client.publish("light", amount)

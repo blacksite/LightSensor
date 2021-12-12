d054756d87d37eaafe2de18bb1b0d57236d94230
jlbrown5-ncat <60116362+jlbrown5-ncat@users.noreply.github.com>
2021-12-12 10:52:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ if __name__ == '__main__':
         client = mqtt.Client()
         client.connect(BROKER_URL, BROKER_PORT)
         # client.publish("topic/test", "Hello world!")
-        client.publish("light", status)
-        client.publish("light", amount)
+        client.publish("light", status, qos=1)
+        client.publish("light", amount, qos=1)
         client.disconnect()
         time.sleep(wait_interval)
 
